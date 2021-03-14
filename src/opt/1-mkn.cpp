9f711125c5d2905d9cc7795af89c939d56be7926
Wei, Zixuan <zixuan.wei@intel.com>
2021-03-14 23:02:09 +0800

    opt-1: permute loop mkn
    Hints: the performance of this version is same to baseline-mkn.
      Compiler could do loop unroll and vectorization within those
      loops.

---
+++ src/opt/1-mkn.cpp
@@ -0,0 +1,50 @@
+
+#include <string>
+
+#include "common/common.hpp"
+#include "common/perf.hpp"
+#include "common/ref_gemm.hpp"
+
+struct opt1 {
+  static constexpr char name[] = "opt1";
+
+  size_t M;
+  size_t N;
+  size_t K;
+
+  float *A;
+  float *B;
+  float *C;
+
+  opt1(float *A, float *B, float *C, const size_t M, const size_t N, const size_t K)
+    : M(M), N(N), K(K), A(A), B(B), C(C) {}
+
+  go::status_t operator()() { return kern(A, B, C, M, N, K); }
+
+  static go::status_t kern(
+      const float *A, const float *B, float *C, const size_t M, const size_t N, const size_t K) {
+    for (int i = 0; i < M; i++) {
+      for (int k = 0; k < K; k++) {
+        float a = A[i * K + k];
+        for (int j = 0; j < N; j += 4) {
+          C[i * N + j] += a * B[k * N + j];
+          C[i * N + j + 1] += a * B[k * N + j + 1];
+          C[i * N + j + 2] += a * B[k * N + j + 2];
+          C[i * N + j + 3] += a * B[k * N + j + 3];
+        }
+      }
+    }
+    return go::status::success;
+  }
+};
+
+int main() {
+  const size_t M = 1280;
+  const size_t N = 1280;
+  const size_t K = 1280;
+  go::GemmPerf<opt1> perf(M, N, K);
+  perf.finalize();
+  std::cout << perf;
+
+  return 0;
+}
