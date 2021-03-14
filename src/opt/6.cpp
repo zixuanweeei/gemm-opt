904f44c3a67fa246acfdb652843fdedfe6a79e54
Wei, Zixuan <zixuan.wei@intel.com>
2021-03-14 22:36:57 +0800

    opt-6: 2D blocking and vectorization

--- src/opt/6.cpp
+++ src/opt/6.cpp
@@ -8,8 +8,9 @@
 #include "immintrin.h"
 #include "xmmintrin.h"
 
-#define BLOCK_SIZE 4
-#define TILE_SIZE 4
+#define BLOCK_SIZE_M 32
+#define BLOCK_SIZE_N 32
+#define TILE_SIZE 16
 
 struct opt6 {
   static constexpr char name[] = "opt6";
@@ -27,10 +28,12 @@ struct opt6 {
 
   go::status_t operator()() { return kern(A, B, C, M, N, K); }
 
-  template <int block_factor = 32, int tile = 32>
+  template <int block_factor_m = 32, int block_factor_n = 32, int tile = 32>
   struct Kern {
-    static constexpr int blk_size = block_factor;
-    static constexpr int iblk_size = block_factor;
+    static constexpr int blk_size_m = block_factor_m;
+    static constexpr int blk_size_n = block_factor_n;
+    static constexpr int SIMD_width = sizeof(__m128) / sizeof(float);
+    static constexpr int blk_SIMD_n = blk_size_n / SIMD_width;
     static constexpr int tile_size = tile;
 
     go::status_t operator()(
@@ -40,34 +43,36 @@ struct opt6 {
 
     static go::status_t kernel(
         const float *A, const float *B, float *C, const size_t M, const size_t N, const size_t K) {
-      const size_t m_outer = M / blk_size;
-      const size_t n_outer = N / blk_size;
-      const size_t k_outer = K / tile_size;
+      const int m_outer = M / blk_size_m;
+      const int n_outer = N / blk_size_n;
+      const int k_outer = K / tile_size;
 
-      for (size_t om = 0; om < m_outer; ++om) {
-        for (size_t on = 0; on < n_outer; ++on) {
-          size_t gm = om * blk_size;
-          size_t gn = on * blk_size;
-          __m128 c[blk_size];
-#pragma GCC ivdep
-          for (size_t blk = 0; blk < blk_size; ++blk) {
-            c[blk] = _mm_setzero_ps();
+      for (int om = 0; om < m_outer; ++om) {
+        for (int on = 0; on < n_outer; ++on) {
+          int gm = om * blk_size_m;
+          int gn = on * blk_size_n;
+          __m128 c[blk_size_m][blk_SIMD_n];
+          for (int blk = 0; blk < blk_size_m; ++blk) {
+            for (int lane = 0; lane < blk_SIMD_n; ++lane) {
+              c[blk][lane] = _mm_setzero_ps();
+            }
           }
-          for (size_t ok = 0; ok < k_outer; ++ok) {
-            size_t gk = ok * tile_size;
-#pragma GCC ivdep
-            for (int im = 0; im < blk_size; ++im) {
-#pragma GCC ivdep
+          for (int ok = 0; ok < k_outer; ++ok) {
+            int gk = ok * tile_size;
+            for (int im = 0; im < blk_size_m; ++im) {
               for (int ik = 0; ik < tile_size; ++ik) {
-                __m128 vec_b = _mm_load_ps(B + (gk + ik) * N + gn);
-                __m128 vec_a_dup = _mm_load_ps1(A + (gm + im) * K + gk + ik);
-                c[im] = _mm_fmadd_ps(vec_a_dup, vec_b, c[im]);
+                for (int lane = 0; lane < blk_SIMD_n; ++lane) {
+                  __m128 vec_b = _mm_load_ps(B + (gk + ik) * N + gn + lane * SIMD_width);
+                  __m128 vec_a_dup = _mm_load_ps1(A + (gm + im) * K + gk + ik);
+                  c[im][lane] = _mm_fmadd_ps(vec_a_dup, vec_b, c[im][lane]);
+                }
               }
             }
           }
-#pragma GCC ivdep
-          for (int m = 0; m < blk_size; ++m) {
-            _mm_store_ps(C + (gm + m) * N + gn, c[m]);
+          for (int m = 0; m < blk_size_m; ++m) {
+            for (int lane = 0; lane < blk_SIMD_n; ++lane) {
+              _mm_store_ps(C + (gm + m) * N + gn + lane * SIMD_width, c[m][lane]);
+            }
           }
         }
       }
@@ -78,7 +83,7 @@ struct opt6 {
 
   static go::status_t kern(
       const float *A, const float *B, float *C, const size_t M, const size_t N, const size_t K) {
-    Kern<BLOCK_SIZE, TILE_SIZE> k;
+    Kern<BLOCK_SIZE_M, BLOCK_SIZE_N, TILE_SIZE> k;
     return k(A, B, C, M, N, K);
   }
 };
